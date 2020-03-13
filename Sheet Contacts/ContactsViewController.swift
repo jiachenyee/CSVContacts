42772da112a579f94d0c63877b4154171e4a3cb3
Jia Chen <36725840+jiaChenCloseParenthesesColon@users.noreply.github.com>
2020-03-13 18:41:02 +0800

    Finished App Mostly

---
+++ Sheet Contacts/ContactsViewController.swift
@@ -0,0 +1,232 @@
+//
+//  ContactsViewController.swift
+//  Sheet Contacts
+//
+//  Created by JiaChen(: on 12/3/20.
+//  Copyright © 2020 jiachen. All rights reserved.
+//
+
+import UIKit
+import ContactsUI
+import CSV
+
+class ContactsViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    var importedContent: String!
+    var csv: CSVReader!
+    var contactValues = ["Name Prefix",
+                         "Given Name",
+                         "Middle Name",
+                         "Family Name",
+                         "Previous Family Name",
+                         "Name Suffix",
+                         "Nickname",
+                         "Phonetic Given Name",
+                         "Phonetic Middle Name",
+                         "Phonetic Family Name",
+                         "Job Title",
+                         "Department Name",
+                         "Organization Name",
+                         "Phonetic Organization Name",
+                         "Postal Code",
+                         "Email Address",
+                         "URL Address",
+                         "Phone Number",
+                         "Note", "--nil--"]
+    var selectedOptions: [String] = []
+    
+    var postalAddresses: [[String]] = []
+    var emailAddresses: [[String]] = []
+    var urlAddresses: [[String]] = []
+    var phoneNumbers: [[String]] = []
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        
+        csv = try! CSVReader(string: importedContent, hasHeaderRow: true)
+        
+        for _ in 0...csv.headerRow!.count - 1 {
+            selectedOptions.append("???")
+            postalAddresses.append([])
+            emailAddresses.append([])
+            urlAddresses.append([])
+            phoneNumbers.append([])
+            
+        }
+    }
+    
+    func saveToContacts() {
+        
+        if selectedOptions.contains("???") {
+            let alert = UIAlertController(title: "Error", message: "Ensure that there are no '???' in the Contact Field column", preferredStyle: UIAlertController.Style.alert)
+            
+            alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+            
+            self.present(alert, animated: true, completion: nil)
+
+            return
+        }
+        
+        var rowCount = 0
+        
+        while let row = csv.next() {
+            let contact = CNMutableContact()
+            
+            for i in 0...row.count - 1 {
+                switch selectedOptions[i] {
+                case "Name Prefix":
+                    contact.namePrefix = row[i]
+                case "Given Name":
+                    contact.givenName = row[i]
+                case "Middle Name":
+                    contact.middleName = row[i]
+                case "Family Name":
+                    contact.familyName = row[i]
+                case "Previous Family Name":
+                    contact.previousFamilyName = row[i]
+                case "Name Suffix":
+                    contact.nameSuffix = row[i]
+                case "Nickname":
+                    contact.nickname = row[i]
+                case "Phonetic Given Name":
+                    contact.phoneticGivenName = row[i]
+                case "Phonetic Middle Name":
+                    contact.phoneticMiddleName = row[i]
+                case "Phonetic Family Name":
+                    contact.phoneticFamilyName = row[i]
+                case "Job Title":
+                    contact.jobTitle = row[i]
+                case "Department Name":
+                    contact.departmentName = row[i]
+                case "Organization Name":
+                    contact.organizationName = row[i]
+                case "Phonetic Organization Name":
+                    contact.phoneticOrganizationName = row[i]
+                case "Postal Code":
+                    
+                    let code = CNMutablePostalAddress()
+                    code.postalCode = row[i]
+                    
+                    let labeledValue = CNLabeledValue(label: postalAddresses[i][0], value: code as CNPostalAddress)
+                    contact.postalAddresses.append(labeledValue)
+                case "Email Address":
+                    
+                    let labeledValue = CNLabeledValue(label: emailAddresses[i][0], value: NSString(string: row[i]))
+                    contact.emailAddresses.append(labeledValue)
+                case "URL Address":
+                    
+                    let labeledValue = CNLabeledValue(label: urlAddresses[i][0], value: NSString(string: row[i]))
+                    contact.urlAddresses.append(labeledValue)
+                case "Phone Number":
+                    let phoneNumber = CNPhoneNumber(stringValue: row[i])
+                    let labeledValue = CNLabeledValue(label: phoneNumbers[i][0], value: phoneNumber)
+                    
+                    contact.phoneNumbers.append(labeledValue)
+                case "Note":
+                    contact.note = row[i]
+                default:
+                    break
+                }
+            }
+            
+            let saveRequest = CNSaveRequest()
+            
+            saveRequest.add(contact, toContainerWithIdentifier: nil)
+            
+            let store = CNContactStore()
+            
+            do {
+                try store.execute(saveRequest)
+            } catch {
+                
+                print(error.localizedDescription)
+            }
+            
+            rowCount += 1
+        }
+        
+    }
+    @IBAction func saveToContactsButtonPressed(_ sender: Any) {
+        
+        saveToContacts()
+        
+        let alert = UIAlertController(title: "Task Complete", message: "Saved to Contacts", preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "OK", style: .default, handler: { (_) in
+            self.dismiss(animated: true)
+        }))
+        
+        self.present(alert, animated: true, completion: nil)
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return csv.headerRow?.count ?? 0
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        
+        cell.textLabel!.text = csv.headerRow![indexPath.row]
+        cell.detailTextLabel!.text = selectedOptions[indexPath.row]
+        cell.selectionStyle = .none
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let alert = UIAlertController(title: "Select a Contact Field", message: nil, preferredStyle: UIAlertController.Style.actionSheet)
+        
+        for i in 0...contactValues.count - 1 {
+            alert.addAction(UIAlertAction(title: contactValues[i], style: .default, handler: { (alertAction) in
+                if ["Postal Code", "Email Address", "URL Address", "Phone Number"].contains(alertAction.title!) {
+                    
+                    DispatchQueue.main.async {
+                        let textAlert = UIAlertController(title: "Label for \(alertAction.title!)", message: "Type a label value for the item. E.g. iPhone, Work", preferredStyle: UIAlertController.Style.alert)
+                        
+                        textAlert.addTextField { (field) in
+                            field.placeholder = "Label value"
+                        }
+                        
+                        textAlert.addAction(UIAlertAction(title: "Done", style: .default, handler: { (action) in
+                            
+                            switch alertAction.title! {
+                            case "Postal Code":
+                                self.postalAddresses[indexPath.row].append(textAlert.textFields![0].text!)
+                            case "Email Address":
+                                self.emailAddresses[indexPath.row].append(textAlert.textFields![0].text!)
+                            case "URL Address":
+                                self.urlAddresses[indexPath.row].append(textAlert.textFields![0].text!)
+                            case "Phone Number":
+                                self.phoneNumbers[indexPath.row].append(textAlert.textFields![0].text!)
+                            default:
+                                break
+                            }
+                        }))
+                        
+                        textAlert.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
+                        
+                        self.present(textAlert, animated: true, completion: nil)
+                    }
+                }
+                self.selectedOptions[indexPath.row] = alertAction.title!
+                tableView.reloadData()
+            }))
+        }
+        
+        alert.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
+        
+        self.present(alert, animated: true, completion: nil)
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- Sheet Contacts/ViewController.swift
+++ Sheet Contacts/ViewController.swift
@@ -8,13 +8,60 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIDocumentPickerDelegate {
 
+    let vc = UIDocumentPickerViewController(documentTypes: [UTI.commaSeparatedText.rawValue], in: .open)
+    var importedContent: String!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        // Set up Files App
+        vc.delegate = self
+        vc.allowsMultipleSelection = false
+        vc.shouldShowFileExtensions = true
     }
+    
+    func documentPicker(_ controller: UIDocumentPickerViewController, didPickDocumentsAt urls: [URL]) {
+        let url = urls.first!
+        
+        var documentName = url.lastPathComponent
+        documentName.removeLast(url.pathExtension.count + 1)
+        
+        if url.pathExtension == "csv" {
+            do {
+                
+                importedContent = try String(contentsOf: url)
+                
+                performSegue(withIdentifier: "contacts", sender: nil)
+            } catch {
+                print(error.localizedDescription)
+                let alert = UIAlertController(title: "Error", message: error.localizedDescription, preferredStyle: UIAlertController.Style.alert)
 
+                alert.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
+                
+                self.present(alert, animated: true, completion: nil)
+            }
+        } else {
+            let alert = UIAlertController(title: "Error", message: "Please select a CSV file.", preferredStyle: UIAlertController.Style.alert)
 
+            alert.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
+            
+            self.present(alert, animated: true, completion: nil)
+        }
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if let dest = segue.destination as? ContactsViewController {
+            
+            dest.importedContent = importedContent
+            
+        }
+    }
+    
+    @IBAction func selectCsvButtonPressed(_ sender: Any) {
+        self.present(vc, animated: true, completion: nil)
+    }
 }
 
